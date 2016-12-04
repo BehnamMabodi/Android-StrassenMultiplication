1df99b5a5c1bf2418234a2b12de0114654f7e80a
Behnam Mabodi <gpxchat@gmail.com>
2016-12-04 20:38:56 +0330

    Removed extra widgets
    and extra codes from previous project
    added transition to new "EnterMatrix" activity

--- app/src/main/java/ir/newway/strassen_multiplication/MainActivity.java
+++ app/src/main/java/ir/newway/strassen_multiplication/MainActivity.java
@@ -1,37 +1,25 @@
 package ir.newway.strassen_multiplication;
 
 import android.content.Context;
-import android.os.Build;
+import android.content.Intent;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
-import android.text.Editable;
-import android.text.TextWatcher;
-import android.view.KeyEvent;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.WindowManager;
-import android.view.inputmethod.EditorInfo;
 import android.view.inputmethod.InputMethodManager;
-import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
 
-import com.newway.newlib.innerClasses.Tools.MyAnimationUtils;
+import ir.newway.strassen_multiplication.InnerClasses.EnterMatrix;
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText mInputEditText;
-    private TextView mResultTextView;
-    private TextView mResultBackground;
-    private TextView mLogTextView;
-
-    private boolean isResultHidden;
-    private IntegerList mList;
-    private Button mSortButton;
+    private EditText mInputEditText_MatrixRow;
+    private EditText mInputEditText_MatrixColumn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -40,58 +28,17 @@ public class MainActivity extends AppCompatActivity {
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        mList = new IntegerList();
-        mSortButton = (Button) findViewById(R.id.button_sort);
-        mResultTextView = (TextView) findViewById(R.id.results);
-        mLogTextView = (TextView) findViewById(R.id.log_textview);
-        mInputEditText = (EditText) findViewById(R.id.MatrixHorizontal);
-        mInputEditText.addTextChangedListener(new TextWatcher() {
-            @Override
-            public void beforeTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-
-            }
-
-            @Override
-            public void onTextChanged(CharSequence charSequence, int i, int i1, int i2) {
-                hideResults();
-            }
-
-            @Override
-            public void afterTextChanged(Editable editable) {
-
-            }
-        });
-        mInputEditText.setOnEditorActionListener(new EditText.OnEditorActionListener() {
-            @Override
-            public boolean onEditorAction(TextView textView, int actionId, KeyEvent keyEvent) {
-                if (actionId == EditorInfo.IME_ACTION_SEARCH || actionId == EditorInfo.IME_ACTION_DONE ||
-                        actionId == EditorInfo.IME_ACTION_GO || actionId == EditorInfo.IME_ACTION_NEXT)
-                    if (!mInputEditText.getText().toString().equals(""))
-                        addNumber(Integer.parseInt(mInputEditText.getText().toString()));
-                    else
-                        Snackbar.make(mLogTextView, R.string.emptyEditText, Snackbar.LENGTH_SHORT).show();
-                return false;
-            }
-        });
-        //  mInputEditText.setTextC
-        mResultBackground = (TextView) findViewById(R.id.result_background_Accepted);
-        //    mResultBackground.setBackgroundColor(getResources().getColor(R.color.colorPrimaryDark));
+        mInputEditText_MatrixRow = (EditText) findViewById(R.id.MatrixRow);
+        mInputEditText_MatrixColumn = (EditText) findViewById(R.id.MatrixColumn);
 
         FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (!mInputEditText.getText().toString().equals(""))
-                    addNumber(Integer.parseInt(mInputEditText.getText().toString()));
+                if (!mInputEditText_MatrixRow.getText().toString().equals("") && !mInputEditText_MatrixColumn.getText().toString().equals(""))
+                    showResults();
                 else
-                    Snackbar.make(mLogTextView, R.string.emptyEditText, Snackbar.LENGTH_SHORT).show();
-            }
-        });
-
-        mSortButton.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                showResults();
+                    Snackbar.make(mInputEditText_MatrixRow, R.string.emptyEditText, Snackbar.LENGTH_SHORT).show();
             }
         });
 
@@ -120,70 +67,22 @@ public class MainActivity extends AppCompatActivity {
         return super.onOptionsItemSelected(item);
     }
 
-    protected void addNumber(Integer number) {
-        mList.addNumber(number);
-        mInputEditText.setText("");
-        String unSortedArrayText = getResources().getString(R.string.unsorted_array);
-        mLogTextView.setText(unSortedArrayText);
-        Integer[] unSortedArray = mList.getUnsortedArray();
-        for (Integer integer : unSortedArray) {
-            mLogTextView.setText(mLogTextView.getText().toString() + integer + "  ");
-        }
-    }
 
     protected void showResults() {
-        isResultHidden = false;
-        mResultTextView.setText(getResources().getString(R.string.log));
-        Integer[] integers = mList.getSortedArray();
-        mLogTextView.setText(mLogTextView.getText().toString() + "\n\n" + getString(R.string.sorted_array) + "\n");
-        for (Integer integer : integers) {
-            mLogTextView.setText(mLogTextView.getText().toString() + integer + "  ");
-        }
-
-        mResultTextView.setTextColor(getResources().getColor(R.color.colorAcceptedDark));
-        mResultBackground.setBackgroundColor(getResources().getColor(R.color.colorAcceptedLight));
-        if (Build.VERSION.SDK_INT >= 21) {
-            getWindow().setNavigationBarColor(getResources().getColor(R.color.colorAcceptedDark));
-            getWindow().setStatusBarColor(getResources().getColor(R.color.colorAcceptedDark));
-        }
-        getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.color.colorAccepted));
-        MyAnimationUtils.enterReveal(mResultBackground, 800, 0, mResultTextView.getWidth() / 2, mResultTextView.getHeight() / 2);
-
+        Intent intent = new Intent(MainActivity.this, EnterMatrix.class);
+        intent.putExtra("matrix_row", Integer.parseInt(mInputEditText_MatrixRow.getText().toString()));
+        intent.putExtra("matrix_column", Integer.parseInt(mInputEditText_MatrixColumn.getText().toString()));
+        startActivity(intent);
     }
 
     protected void hideResults() {
-        if (!isResultHidden) {
-            isResultHidden = true;
-            mList.clearList();
-            mResultTextView.setText(getResources().getString(R.string.result_text_enter_string));
-            mLogTextView.setText(getResources().getString(R.string.input_hint_full));
-            mResultTextView.setTextColor(getResources().getColor(R.color.colorPrimaryDark));
-            mResultBackground.setBackgroundColor(getResources().getColor(R.color.colorPrimaryLight));
-            if (Build.VERSION.SDK_INT >= 21) {
-                getWindow().setNavigationBarColor(getResources().getColor(R.color.colorPrimaryDark));
-                getWindow().setStatusBarColor(getResources().getColor(R.color.colorPrimaryDark));
-            }
-            getSupportActionBar().setBackgroundDrawable(getResources().getDrawable(R.color.colorPrimary));
-            MyAnimationUtils.enterReveal(mResultBackground, 800, 0, mResultTextView.getWidth() / 2, mResultTextView.getHeight() / 2);
-        }
-    }
-
-    @Override
-    public void onBackPressed() {
-        if (isResultHidden)
-            super.onBackPressed();
-        else {
-            mInputEditText.setText("");
-            hideResults();
-            hideSoftKey();
-        }
     }
 
     protected void hideSoftKey() {
-        mInputEditText.clearFocus();
+        mInputEditText_MatrixRow.clearFocus();
 
         getWindow().setSoftInputMode(WindowManager.LayoutParams.SOFT_INPUT_STATE_ALWAYS_HIDDEN);
         InputMethodManager imm = (InputMethodManager) getSystemService(Context.INPUT_METHOD_SERVICE);
-        imm.hideSoftInputFromWindow(mInputEditText.getWindowToken(), 0);
+        imm.hideSoftInputFromWindow(mInputEditText_MatrixRow.getWindowToken(), 0);
     }
 }
