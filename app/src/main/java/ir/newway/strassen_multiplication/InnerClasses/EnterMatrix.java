80994e5f905db84e8ed424408c87d59ee782a9d1
Behnam Mabodi <gpxchat@gmail.com>
2016-12-04 20:37:23 +0330

    "Enter Matrix" Activity created

---
+++ app/src/main/java/ir/newway/strassen_multiplication/InnerClasses/EnterMatrix.java
@@ -0,0 +1,49 @@
+package ir.newway.strassen_multiplication.InnerClasses;
+
+import android.os.Bundle;
+import android.support.design.widget.FloatingActionButton;
+import android.support.design.widget.Snackbar;
+import android.support.v7.app.AppCompatActivity;
+import android.support.v7.widget.Toolbar;
+import android.view.MenuItem;
+import android.view.View;
+
+import ir.newway.strassen_multiplication.R;
+
+public class EnterMatrix extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_enter_matrix);
+
+        int matrixRow = getIntent().getExtras().getInt("matrix_row");
+        int matrixcolumn = getIntent().getExtras().getInt("matrix_column");
+
+
+        Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
+        setSupportActionBar(toolbar);
+        getSupportActionBar().setDisplayShowHomeEnabled(true);
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+
+        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
+        fab.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
+                        .setAction("Action", null).show();
+            }
+        });
+        getSupportActionBar().setDisplayHomeAsUpEnabled(true);
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        int id = item.getItemId();
+        if (id == android.R.id.home) {
+            finish();
+            return true;
+        }
+        return super.onOptionsItemSelected(item);
+    }
+}
